4bd48a045a3298b4d290382cda5b561b7953789e
Jason Swett <jason@mhcare.com>
2022-04-13 22:15:17 -0400

    Change answer language to pinyin.

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -2,10 +2,10 @@ class FlashCardsController < ApplicationController
   def index
     @word = Word.all.sample
 
-    english_words = Word.all.map(&:english)
-    @options = [@word.english]
-    @options[1] = (english_words - @options).sample
-    @options[2] = (english_words - @options).sample
+    pinyin_words = Word.all.map(&:pinyin)
+    @options = [@word.pinyin]
+    @options[1] = (pinyin_words - @options).sample
+    @options[2] = (pinyin_words - @options).sample
     @options.shuffle!
   end
 end
