ee3e128e87a0f732f62c97e50f9afca135da0396
Jason Swett <jason@mhcare.com>
2022-04-14 17:12:32 -0400

    Refactoring.

--- app/javascript/controllers/answers_controller.js
+++ app/javascript/controllers/answers_controller.js
@@ -4,7 +4,11 @@ export default class extends Controller {
   select(e) {
     e.preventDefault()
     e.target.style.backgroundColor = "#F00"
-    document.getElementsByClassName("correct-answer")[0].style.backgroundColor = "#0F0"
+    this.correctAnswerElement().style.backgroundColor = "#0F0"
     setTimeout(() => { window.location = e.target.href }, 500)
+  }
+
+  correctAnswerElement() {
+    return document.getElementsByClassName("correct-answer")[0]
   }
 }
