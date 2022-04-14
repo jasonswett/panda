409f6a5239cdf7dbc31e20d7ae17eadf28922fb6
Jason Swett <jason@mhcare.com>
2022-04-14 03:13:35 -0400

    Protect /words.

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,6 @@
 class WordsController < ApplicationController
   before_action :set_word, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /words or /words.json
   def index
