81657cb8ca669b6e5d69e2697d6d6ad64467df74
Jason Swett <jason@mhcare.com>
2022-04-13 20:21:02 -0400

    Improve options.

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -2,6 +2,10 @@ class FlashCardsController < ApplicationController
   def index
     @word = Word.all.sample
 
-    @options = [@word.english, "deputy", "croissant"].shuffle
+    english_words = Word.all.map(&:english)
+    @options = [@word.english]
+    @options[1] = (english_words - @options).sample
+    @options[2] = (english_words - @options).sample
+    @options.shuffle!
   end
 end
