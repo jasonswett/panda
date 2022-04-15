fba4ba2c46d9a37c074e31e92c4875a8c1c2315d
Jason Swett <jason@mhcare.com>
2022-04-15 19:36:46 -0400

    Add About page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :phrases
   resources :answers, only: :create
+  get "/about", to: "static_pages#about"
   root "flash_cards#index"
 end
