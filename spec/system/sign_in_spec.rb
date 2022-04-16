efbe70f2e7f995807875ea1dcc264015a25dfb8f
Jason Swett <jason@mhcare.com>
2022-04-15 20:08:11 -0400

    Add test for bad login.

--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -6,15 +6,23 @@ RSpec.describe "Signing in", type: :system do
   before { create(:phrase) }
 
   context "with valid credentials" do
-    describe "using username" do
-      it "signs the user in" do
-        user = create(:user)
-        visit new_user_session_path
-        fill_in "Email", with: user.email
-        fill_in "Password", with: user.password
-        click_on "Sign In"
-        expect(page).to have_content("Sign out")
-      end
+    it "signs the user in" do
+      user = create(:user)
+      visit new_user_session_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_on "Sign In"
+      expect(page).to have_content("Sign out")
     end
 	end
+
+  context "with invalid credentials" do
+    it "shows an error message" do
+      visit new_user_session_path
+      fill_in "Email", with: "foo@bar.com"
+      fill_in "Password", with: "bar"
+      click_on "Sign In"
+      expect(page).to have_content("Invalid Email or password")
+    end
+  end
 end
